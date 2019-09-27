abe4f666760274110f6124e966b17446096172f2
patchcasey <patchcasey@gmail.com>
2019-09-26 23:12:32 -0400

    progress on parquet

--- pset_1/__main__.py
+++ pset_1/__main__.py
@@ -1,16 +1,21 @@
-from .hash_str import get_csci_salt, get_user_id, hash_str
+from hash_str import get_csci_salt, get_user_id, hash_str
+import pandas as pd
 
 
 def get_user_hash(username, salt=None):
     salt = salt or get_csci_salt()
+    print(salt)
     return hash_str(username, salt=salt)
 
 
 if __name__ == "__main__":
 
-    for user in ["gorlins", "<YOUR_GITHUB_USERNAME>"]:
+    for user in ["patch", "patchcasey"]:
         print("Id for {}: {}".format(user, get_user_id(user)))
 
-    data_source = "data/hashed.xlsx"
+
+    data_source = 'C:/aPost_Grad/Fall_Class/pset1branch/2019fa-pset-1-patchcasey/data/hashed.xlsx'
+    df = pd.read_excel(data_source)
+    df.to_parquet('hashed.parquet', engine='pyarrow')
 
     # TODO: read in, save as new parquet file, read back just id column, print
